f57ff729b6bd0dca6205223d2c8eddadf1c46d41
Naseem Khan <naseemsaffy@gmail.com>
2024-06-28 14:16:57 +0500

    chore: Connect to MongoDB in server startup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 import { config } from "./src/config/config.js";
 
 const startServer = async () => {
   try {
+    connectDB();
+
     app.listen(config.port, () => {
       console.log(`Server is running on port ${config.port}`);
     });
