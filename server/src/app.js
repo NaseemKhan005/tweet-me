85012720e4d4344c7b28ecdce92b35861825e6f6
Naseem Khan <naseemsaffy@gmail.com>
2024-06-28 14:37:38 +0500

    chore: Update app.js with middlewares for JSON parsing, URL encoding, cookie parsing, and request logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,14 @@
 import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Server is running...</h1>");
