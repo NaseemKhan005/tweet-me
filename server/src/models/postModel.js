5bc0acea3dbb666e62cd42fb24a169a012d77101
Naseem Khan <naseemsaffy@gmail.com>
2024-06-29 13:18:08 +0500

    feat: Add post model for creating and managing posts

---
+++ server/src/models/postModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
