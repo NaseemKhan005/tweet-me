fa6955799444ff0ccc3b69a4aa2a586489ba1306
Naseem Khan <naseemsaffy@gmail.com>
2024-07-05 00:35:41 +0500

    feat: Add notification page and related components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
 import RootLayout from "./layouts/RootLayout";
 import AuthLayout from "./layouts/AuthLayout";
+import NotificationPage from "./pages/notification/NotificationPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="notifications" element={<NotificationPage />} />
         </Route>
 
         <Route path="/auth" element={<AuthLayout />}>
